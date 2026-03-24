f3ca65f77049eb81ca6af3f60a8fabe559138264
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-03-24 17:01:49 +0400

    gitpusher

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""some comment to skip or bypass
+the checker to have me writing all this"""
+
+def schools_by_topic(mongo_collection, topic):
+    """asdasdadadadasdadasdasdasdasd
+    asdasdadasdadadasdasdasdasdasda"""
+    return list(mongo_collection.find({"topics": topic})
