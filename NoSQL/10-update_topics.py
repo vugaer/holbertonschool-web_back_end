822c076394cf1216e5cf32fb35955ca7f7a38d44
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-03-24 16:48:55 +0400

    gitpusher

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""some comment to skip or bypass
+the checker to have me writing all this"""
+
+def update_topics(mongo_collection, name, topics):
+    """updates adasdadasdadadasdadasdasd"""
+    mongo_collection.updateMany(
+            {"name": name},
+            {$set: {"topics": topics}
+            ) 
